d0b3d034f323498dfca98e92100891559bebc8c8
Divyajyoti Panda <pandadivya02@gmail.com>
2024-08-04 18:03:03 +0530

    Add search trigger

--- index.js
+++ index.js
@@ -32,14 +32,17 @@ app.get("/", async (req, res) => {
     let response = await db.query("SELECT * FROM notes;");
     let noteEntries = response.rows;
     let queryResults = [];
+    let queryResultsCoverId = [];
     if (Object.hasOwn(req.query, 'q')) {queryText = req.query.q;}
     if (queryText !== "") {
         response = await axios.get(`https://openlibrary.org/search.json?q=${queryText}&limit=5`);
         queryResults = response.data.docs
         // console.log(queryResults[0].cover_i);
     }
+    queryResults.forEach((elem) => {queryResultsCoverId.push(elem.cover_i);});
     res.render("main.ejs", {
-        title: "Book Notes", activeTab: "home", queryText: queryText, queryResults: queryResults, noteEntries: noteEntries
+        title: "Book Notes", activeTab: "home", queryText: queryText, queryResults: queryResults, 
+        queryResultsCoverIdJSON: JSON.stringify(queryResultsCoverId), noteEntries: noteEntries
     });
 });
 
